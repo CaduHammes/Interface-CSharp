b4f76097fd1f4bb20295273199c787617196a913
Cadu Hammes <duduhammes@hotmail.com>
2021-11-18 17:10:47 -0300

    Solucao sem interface - PARTE 1

--- Interfaces/Interfaces/Program.cs
+++ Interfaces/Interfaces/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Interfaces.Entities;
+using System;
+using System.Globalization;
 
 namespace Interfaces
 {
@@ -6,7 +8,15 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter rental data: ");
+            Console.WriteLine("Car model");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
         }
     }
 }
---
+++ Interfaces/Interfaces/Services/RentalService.cs
@@ -0,0 +1,25 @@
+﻿using Interfaces.Entities;
+
+namespace Interfaces.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService()
+        {
+        }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+
+        }
+    }
+}
